b2a0eb35f91235eb3bb01d646f50ac49972e8b91
Mandar Rajendra Sankhe <45758847+MandarSankhe@users.noreply.github.com>
2025-03-09 01:23:40 -0500

    update 4

--- backend/server.js
+++ backend/server.js
@@ -277,7 +277,7 @@ async function startServer() {
   await connectToDatabase();
 
   // app.listen(PORT, () => {
-  //   console.log(`Server running at http://localhost:${PORT}/graphql`);
+  //   console.log(`Server running at http://localhost:${PORT}/graphql`); test
   // });
 }
 
